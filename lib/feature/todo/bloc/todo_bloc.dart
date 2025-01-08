cf88df6961e9d4e6e3c01e3134ad992f2bfab85a
Watchakorn Buddeewong <watchakorn.fakduai@gmail.com>
2025-01-08 23:04:58 +0700

    feat : add standard

---
+++ lib/feature/todo/bloc/todo_bloc.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_app_standard/domain/datasource/app_datebase.dart';
+import 'package:mobile_app_standard/domain/repositories/toda_repo.dart';
+
+part 'todo_event.dart';
+part 'todo_state.dart';
+
+class TodoBloc extends Bloc<TodoEvent, TodoState> {
+  final TodoRepository todoRepository;
+
+  TodoBloc({required this.todoRepository}) : super(TodoInitial()) {
+    on<LoadTodos>((event, emit) async {
+      emit(TodoLoading());
+      try {
+        final todos = await todoRepository.getAllTodoItems();
+        if (kDebugMode) {
+          print('Loaded todos: $todos');
+        }
+        emit(TodoLoaded(todos));
+      } catch (e) {
+        if (kDebugMode) {
+          print('Failed to load todos: $e');
+        }
+        emit(TodoError('Failed to load todos.'));
+      }
+    });
+
+    on<AddTodo>((event, emit) async {
+      emit(TodoLoading());
+
+      try {
+        // เพิ่ม Todo ใหม่
+        await todoRepository.addTodoItem(event.title, event.content);
+
+        // โหลดข้อมูลใหม่หลังจากเพิ่ม
+        final todos = await todoRepository.getAllTodoItems();
+        emit(TodoLoaded(todos));
+      } catch (e) {
+        // ส่งข้อผิดพลาดไปที่ state
+        emit(TodoError('Failed to add todo: ${e.toString()}'));
+      }
+    });
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mobile_app_standard/domain/http_client/ip.dart';
+import 'package:mobile_app_standard/domain/http_client/websocket.dart';
+import 'package:mobile_app_standard/locator.dart';
+import 'package:mobile_app_standard/router/router.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupLocator();
+  try {
+    await dotenv.load(fileName: '.env.local');
+    print('Loaded .env file');
+  } catch (e) {
+    print('Error loading .env file: $e');
+  }
+
+  connectHttpClient();
+  connectWebSocket();
+
+  runApp(
+    const MyApp(),
+  );
+}
+
+Future<void> connectHttpClient() async {
+  final ip = await IpClient().getIp();
+  if (kDebugMode) {
+    print('HttpClient: IP: $ip');
+  }
+}
+
+Future<void> connectWebSocket() async {
+  final wsClient = WebSocketClient();
+
+  // เชื่อมต่อไปยัง WebSocket
+  wsClient.connect();
+
+  // ส่งข้อความไปยัง WebSocket
+  wsClient.sendMessage('Hello, WebSocket!');
+
+  // ฟังการตอบกลับจาก WebSocket
+  wsClient.messages.listen((message) {
+    print('Received: $message');
+  });
+
+  // หลังจาก 5 วินาที ปิดการเชื่อมต่อ
+  Future.delayed(Duration(seconds: 5), () {
+    wsClient.close();
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      onGenerateRoute:
+          AppRouter().onGenerateRoute, // ใช้ AppRouter เพื่อจัดการเส้นทาง
+      initialRoute: '/', // หน้าแรกจะเป็น HomePage
+    );
+  }
+}
