22bb5bf833df36757a132dfacb25b84dff459360
Watchakorn Buddeewong <watchakorn.fakduai@gmail.com>
2025-03-22 01:35:04 +0700

    feat: remove unused import for app_database in todo_page.dart

--- lib/feature/todo/pages/todo_page.dart
+++ lib/feature/todo/pages/todo_page.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mobile_app_standard/domain/datasource/app_datebase.dart';
 import 'package:mobile_app_standard/feature/todo/bloc/todo_bloc.dart';
 import 'package:mobile_app_standard/feature/todo/model/todo_model.dart';
 import 'package:mobile_app_standard/feature/todo/widgets/dialog/add_todo_dialog.dart';
